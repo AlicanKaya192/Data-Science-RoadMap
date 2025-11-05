ae92dd06d9005c86fa9264322d883d36a5990a09
AlicanONI.Kaya <alicankaya268@gmail.com>
2025-11-05 01:45:02 +0100

    Müşteri yaşam Boyu Değeri cltv tamamlandı.

--- CRM Analitik/Müşteri_Yaşam_Boyu_Değeri_ve_Tahmini/Yaşam_Boyu_Değeri/cltv.py
+++ CRM Analitik/Müşteri_Yaşam_Boyu_Değeri_ve_Tahmini/Yaşam_Boyu_Değeri/cltv.py
@@ -39,20 +39,26 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 pd.set_option('display.float_format', lambda x: '%.5f' % x)
 
+# Veri setinin okunması
 df_ = pd.read_excel("Datasets/Müşteri_Yaşam_Boyu_Değeri_Dataset/online_retail_II.xlsx", sheet_name='Year 2009-2010')
 df = df_.copy()
 df.head()
 df.isnull().sum()
 
+# İptal edilen faturaların çıkarılması
 df = df[~df["Invoice"].str.contains("C", na=False)]
 df.describe().T
 
+# Negatif miktarlı ürünlerin kaldırılması
 df = df[df["Quantity"] > 0]
 
+# Eksik verilerin silinmesi
 df.dropna(inplace=True)
 
+# Toplam fiyat değişkeni
 df["Total_Price"] = df["Quantity"] * df["Price"]
 
+# Müşteri bazında temel istatistiklerin oluşturulması
 cltv_c = df.groupby("Customer ID").agg({"Invoice": lambda x: x.nunique(),
                                         "Quantity": lambda x: x.sum(),
                                         "TotalPrice": lambda x: x.sum()})
@@ -65,7 +71,8 @@ cltv_c.columns = ["total_transaction", "total_unit", "total_price"]
 
 cltv_c.head()
 
-cltv_c["average_order_value"] =cltv_c["total_price"] / cltv_c["total_transaction"]
+# Ortalama sipariş değerinin hesaplanması
+cltv_c["average_order_value"] = cltv_c["total_price"] / cltv_c["total_transaction"]
 
 
 #####################################################################################
@@ -74,6 +81,7 @@ cltv_c["average_order_value"] =cltv_c["total_price"] / cltv_c["total_transaction
 
 cltv_c.head()
 
+# Satın alma sıklığının hesaplanması
 cltv_c["purchase_frequency"] = cltv_c["total_transaction"] / cltv_c.shape[0]
 
 
@@ -81,8 +89,10 @@ cltv_c["purchase_frequency"] = cltv_c["total_transaction"] / cltv_c.shape[0]
 # 4. Repeat Rate & Churn Rate (birden fazla alışveriş yapan müşteri sayısı / tüm müşteriler)
 #####################################################################################
 
+# Tekrar alışveriş oranı
 repeat_rate = cltv_c[cltv_c["total_transaction"] > 1].shape[0] / cltv_c.shape[0]
 
+# Müşteri kayıp oranı
 churn_rate = 1 - repeat_rate
 
 
@@ -90,6 +100,7 @@ churn_rate = 1 - repeat_rate
 # 5. Profit Margin (profit_margin = total_price * 0.10)
 #####################################################################################
 
+# Kar marjı hesaplanması
 cltv_c["profit_margin"] = cltv_c["total_price"] * 10
 
 
@@ -97,6 +108,7 @@ cltv_c["profit_margin"] = cltv_c["total_price"] * 10
 # 6. Customer Value (customer_value = average_order_value * purchase_frequency)
 #####################################################################################
 
+# Müşteri değerinin hesaplanması
 cltv_c["customer_value"] = cltv_c["average_order_value"] * cltv_c["purchase_frequency"]
 
 
@@ -104,8 +116,10 @@ cltv_c["customer_value"] = cltv_c["average_order_value"] * cltv_c["purchase_freq
 # 7. Customer Lifetime Value (CLTV = (customer_value / churn_rate) x profit_margin)
 #####################################################################################
 
+# CLTV hesaplanması
 cltv_c["cltv"] = (cltv_c["customer_value"] / churn_rate) * cltv_c["profit_margin"]
 
+# CLTV değeri en yüksek müşteriler
 cltv_c.sort_values(by="cltv", ascending=False).head()
 
 
@@ -113,28 +127,32 @@ cltv_c.sort_values(by="cltv", ascending=False).head()
 # 8. Segmentlerin Oluşturulması
 #####################################################################################
 
-cltv_c.sort_values(by="cltv", ascending=False).head()
-cltv_c.sort_values(by="cltv", ascending=False).tail()
-
+# Segmentlerin oluşturulması
 cltv_c["segment"] = pd.qcut(cltv_c["cltv"], q=4, labels=["D", "C", "B", "A"])
 
-cltv_c.sort_values(by="cltv", ascending=False).head()
+# Segment bazında analiz
+cltv_c.groupby("segment").agg({"cltv": ["count", "mean", "sum"]})
 
-cltv_c.groupby("segment").agg({"count", "mean", "sum"})
-
+# Sonuçların kaydedilmesi
 cltv_c.to_csv("cltv_c.csv")
+
 
 #####################################################################################
 # 9. BONUS: Tüm İşlemlerin Fonksiyonlaştırılması
 #####################################################################################
 
 def create_cltv_c(dataframe, profit=0.10):
+    """
+    Verilen veri setinden müşteri yaşam boyu değerini (CLTV) hesaplayan fonksiyon.
+    """
 
     # Veriyi Hazırlama
     dataframe = dataframe[~dataframe["Invoice"].str.contains("C", na=False)]
     dataframe = dataframe[(dataframe["Quantity"] > 0)]
     dataframe.dropna(inplace=True)
     dataframe["TotalPrice"] = dataframe["Quantity"] * dataframe["Price"]
+
+    # Müşteri bazında özet değerlerin oluşturulması
     cltv_c = dataframe.groupby("Customer ID").agg({"Invoice": lambda x: x.nunique(),
                                                    "Quantity": lambda x: x.sum(),
                                                    "TotalPrice": lambda x: x.sum()})
@@ -165,6 +183,6 @@ def create_cltv_c(dataframe, profit=0.10):
     return cltv_c
 
 
+# Fonksiyonun çalıştırılması
 df = df_.copy()
-
 clv = create_cltv_c(df)
